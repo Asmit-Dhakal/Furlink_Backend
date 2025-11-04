a8d0ad2bdeb2322c961f4b34d4876102858b9290
Asmit-Dhakal <asmitdhakal369@gmail.com>
2025-11-04 19:33:53 +0545

    Update `gallery` URLs to register `PetImageViewSet` at root endpoint

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import PetImageViewSet
 
 router = DefaultRouter()
-router.register(r'gallery', PetImageViewSet, basename='gallery')
+router.register(r'', PetImageViewSet, basename='gallery')
 
 urlpatterns = router.urls
